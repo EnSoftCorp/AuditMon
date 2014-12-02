dfeb4e09e45bd5aa5085c6e3c647097971c39fb7
benjholla <benjholla@gmail.com>
2014-12-02 15:11:06 -0600

    update comments

--- toolbox.shell/shellInit.scala
+++ toolbox.shell/shellInit.scala
@@ -1,8 +1,8 @@
 /*
- * This is an initialization file for the Atlas Interpreter view.  Each time an Interpreter linked
+ * This is an initialization file for the Atlas Shell.  Each time a Shell linked
  * with this project is opened or restarted, the code in this file will be run as scala code.  Below
- * is included the default initialization code for the interpreter.  As long as this file exists only
- * the code in this file will be run on interpreter startup; this default code will not be run if you
+ * is included the default initialization code for the Shell.  As long as this file exists only
+ * the code in this file will be run on Shell startup; this default code will not be run if you
  * remove it from this file.
  * 
  * You do not need to put initialization code in a scala object or class.
